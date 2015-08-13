57e05e515e4069cf52e47b0b1418d9fb575a4761
Kyle Fuller <kyle@fuller.li>
2015-08-13 20:02:31 +0100

    Refactor observable tests

--- observable_test.go
+++ observable_test.go
@@ -3,6 +3,7 @@ package GoReactive
 
 import (
   "testing"
+  "time"
   "github.com/stvp/assert"
 )
 
@@ -20,67 +21,66 @@ func oneToFiveObservable() Observable {
 }
 
 
+/// Subscribes to an observable returning once it completes or fails
+func wait(t *testing.T, observable Observable) []interface{} {
+  values := []interface{}{}
+  completed := false
+  failed := false
+
+  observable.Subscribe(
+      func(value interface{}) { values = append(values, value) },
+      func() { completed = true },
+      func(err error) { failed = true })
+
+  for !completed && !failed {
+    time.Sleep(500 * time.Millisecond)
+  }
+
+  if failed {
+    t.FailNow()
+  }
+
+  return values
+}
+
+
 func TestSkipSkipsValues(t *testing.T) {
   observable := oneToFiveObservable()
-  skippedObservable := Skip(observable, 2)
+  values := wait(t, Skip(observable, 2))
 
-  values := []int{}
-  skippedObservable.Subscribe(
-      func(value interface{}) { values = append(values, value.(int)) },
-      func() {},
-      func(err error) {})
-
-  assert.Equal(t, values, []int{3, 4, 5})
+  assert.Equal(t, values, []interface{}{3, 4, 5})
 }
 
 
 func TestMap(t *testing.T) {
   observable := oneToFiveObservable()
-
   mappedObservable := Map(observable, func(value interface{}) interface{} {
     return value.(int) * 2
   })
+  values := wait(t, mappedObservable)
 
-  values := []int{}
-  mappedObservable.Subscribe(
-      func(value interface{}) { values = append(values, value.(int)) },
-      func() {},
-      func(err error) {})
-
-  assert.Equal(t, values, []int{2, 4, 6, 8, 10})
+  assert.Equal(t, values, []interface{}{2, 4, 6, 8, 10})
 }
 
 
 func TestFilter(t *testing.T) {
   observable := oneToFiveObservable()
-
   filteredObservable := Filter(observable, func(value interface{}) bool {
     return (value.(int) % 2) == 0
   })
+  values := wait(t, filteredObservable)
 
-  values := []int{}
-  filteredObservable.Subscribe(
-      func(value interface{}) { values = append(values, value.(int)) },
-      func() {},
-      func(err error) {})
-
-  assert.Equal(t, values, []int{2, 4})
+  assert.Equal(t, values, []interface{}{2, 4})
 }
 
 
 func TestExclude(t *testing.T) {
   observable := oneToFiveObservable()
-
   filteredObservable := Exclude(observable, func(value interface{}) bool {
     return (value.(int) % 2) == 0
   })
+  values := wait(t, filteredObservable)
 
-  values := []int{}
-  filteredObservable.Subscribe(
-      func(value interface{}) { values = append(values, value.(int)) },
-      func() {},
-      func(err error) {})
-
-  assert.Equal(t, values, []int{1, 3, 5})
+  assert.Equal(t, values, []interface{}{1, 3, 5})
 }
 
